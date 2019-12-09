f69faefa914a1ce1dfe50510985f79c21ecbc364
jiangt <jiangt@miiteen.com>
2019-12-09 21:49:45 +0800

    首页基础布局

--- pages/index/index.vue
+++ pages/index/index.vue
@@ -1,36 +1,92 @@
 <template>
 	<view class="content">
 		<!-- 轮播图-->
-		<view class="uni-padding-wrap">
-			<view class="page-section swiper">
-				<view class="page-section-spacing">
-					<swiper class="swiper" indicator-dots="true" autoplay="true" interval="2000" duration="500" indicator-active-color="#133c8e" indicator-color="#999999">
-						<swiper-item><view class="swiper-item">A</view></swiper-item>
-						<swiper-item><view class="swiper-item">B</view></swiper-item>
-						<swiper-item><view class="swiper-item">C</view></swiper-item>
-						<!-- 											<swiper-item v-for="(item , index) in homeSlide" :key="index">
-															<image :src="item.img" mode="aspectFill"></image>
-														</swiper-item> -->
-					</swiper>
-				</view>
+		<view class="page-section swiper">
+			<view class="page-section-spacing">
+				<swiper class="swiper" indicator-dots="true" autoplay="true" interval="2000" duration="500" indicator-color="#999999" indicator-active-color="#133C8E">
+					<swiper-item>
+						<view class="swiper-item"><image src="../../static/swiper.png"></image></view>
+					</swiper-item>
+					<swiper-item><view class="swiper-item">B</view></swiper-item>
+					<swiper-item><view class="swiper-item">C</view></swiper-item>
+				</swiper>
 			</view>
 		</view>
 		<!-- 按钮组-->
-		<view class="mt-buttons-box">
-			<view class="mt-button-item">
-				<view class="mt-image-content">
-					<image src="../../static/抢单.png" ></image>
-				</view>
-				<text>抢单记录(1)</text>
+		<view class="mt-buttons-content">
+			<view class="mt-buttons-item current-active">
+				<view class="mt-image-content second-image">
+					<image class="mt-order-img" src="../../static/抢单.png"></image></view>
+				<text class="mt-order">抢单记录(1)</text>
 			</view>
-			<view class="mt-button-item">
-				<view class="mt-image-content">
-					<image src="../../static/抢单.png" ></image>
-				</view>
-				<text>抛单记录(0)</text>
+			<view class="mt-place-content"></view>
+			<view class="mt-buttons-item"  @tap="fn">
+				<view class="mt-image-content second-image">
+					<image class="mt-order-img" src="../../static/扔单.png"></image></view>
+				<text class="mt-order">扔单记录(0)</text>
 			</view>
 		</view>
-		<!-- 按钮组end -->
+		<!-- 抢单信息 -->
+			<view class="mt-throw-content">
+				<view class="mt-subject">
+					
+					<view class="mt-start-image">
+						<image class="mt-city" src="../../static/起点.png"></image>
+						<text style="font-size: 30rpx;font-weight: 700;">苏州市</text>
+						<image class="mt-detial" src="../../static/起点.png"></image>
+					</view>
+					<view class="mt-start-image">
+						<image class="mt-city" src="../../static/起点.png"></image>
+						<text style="font-size: 30rpx;font-weight: 700;">苏州市</text>
+					</view>
+					<view class="mt-front">
+						<text class="mt-front-one">订单金额：40元</text>
+						<text class="mt-front-two">订单金额：20元</text>
+					</view>
+					<view class="mt-distance">
+						<text class="mt-distance-icon"></text>
+						<text class="mt-distance-text">距离：6公里</text>
+					</view>
+					<view class="mt-distance">
+						<text class="mt-distance-icon"></text>
+						<text class="mt-distance-text">出发时间：2019-05-20</text>
+					</view>
+					<view class="mt-distance">
+						<text class="mt-distance-icon"></text>
+						<text class="mt-distance-text">搬运物品：爱心一枚</text>
+					</view>
+					<view class="mt-label">
+						<text class="mt-label-car">中面包车</text>
+						<text class="mt-label-car">多拍车</text>
+					</view>
+					<view class="mt-seedeil">
+						<text class="mt-seedeil-text">此鼎带你武都区武都区武都区我</text>
+						<button class="mt-seedeil-btn">查看</button>
+					</view>
+				</view>
+				<!-- er -->
+				<view class="mt-subject1">
+					<view class="mt-start-image">
+						<image class="mt-city" src="../../static/起点.png"></image>
+						<text style="font-size: 30rpx;font-weight: 700;">苏州市</text>
+						<image class="mt-detial" src="../../static/起点.png"></image>
+					</view>
+					<view class="mt-start-image">
+						<image class="mt-city" src="../../static/起点.png"></image>
+						<text style="font-size: 30rpx;font-weight: 700;">苏州市</text>
+					</view>
+					<view class="mt-distance">
+						<text class="mt-distance-icon"></text>
+						<text class="mt-distance-text">距离：6公里</text>
+					</view>
+					<view class="mt-distance">
+						<text class="mt-distance-icon"></text>
+						<text class="mt-distance-text">出发时间：2019-05-20</text>
+						<button class="mt-seedeil-btn">查看</button>
+					</view>
+				</view>
+			</view>
+		</view>
 	</view>
 </template>
 
@@ -42,20 +98,142 @@ export default {
 		};
 	},
 	onLoad() {},
-	methods: {}
+	methods: {
+		fn: function (){
+			//跳转到扔单记录
+			uni.navigateTo({
+			    url: "../order/throwRecord"
+			});
+		}
+	}
+	
 };
 </script>
 
 <style>
-	.swiper {
-		height:100%;
-	}
-	.uni-swiper-wrapper {
-		height:100%;
-	}
-	.page-section-spacing {
-		height:100%;
-		}
+page {
+	background-color: #f4f4f4;
+}
+image {
+	width: 100%;
+	height: 100%;
+}
+.mt-seedeil {
+	width: 100%;
+	height: 70rpx;
+	position: relative;
+}
+.mt-seedeil-text {
+	margin-left: 30rpx;
+	display: inline-block;
+	font-size: 24rpx;
+	
+}
+.mt-seedeil-btn {
+	width: 120rpx;
+	height: 50rpx;
+	display: inline-block;
+	text-align: center;
+	line-height: 50rpx;
+	border-radius: 25rpx;
+	background-color: skyblue;
+	color: #fff;
+	font-size: 30rpx;
+	margin-left: 60rpx;
+	position: absolute;
+	right: 100rpx;
+	/* top: 10rpx; */
+}
+.mt-label {
+	width: 100%;
+	
+	height: 50rpx;
+	color: orange;
+	font-size: 20rpx;
+}
+.mt-label-car{
+	display: inline-block;
+	/* background-color: orange; */
+	width: 130rpx;
+	 /* height: 30rpx; */
+	text-align: center;
+	border: 1rpx solid orange;
+	border-radius: 10rpx;
+	margin-right: 10rpx;
+	margin-left: 30rpx;
+}
+.mt-distance {
+	position: relative;
+	width: 100%;
+	height: 50rpx;
+	color: #ccc;
+	font-size: 24rpx;
+}
+.mt-distance-icon {
+	display: inline-block;
+	width: 20rpx;
+	height: 20rpx;
+	margin-left: 10rpx;
+	margin-right: 10rpx;
+	background-color: pink;
+	
+}
+.mt-front {
+	width: 100%;
+	height: 60rpx;
+	font-size: 20rpx;
+	font-weight: 700;
+}
+.mt-front-one {
+	padding-left: 20rpx;
+}
+.mt-front-two {
+	padding-left: 90rpx;
+}
+.mt-detial{
+	float: right;
+	margin: 10rpx 20rpx 0 0;
+	width: 50rpx;
+	height: 50rpx;
+	background-color: pink;
+}
+
+.mt-start-image {
+	width: 100%;
+	height: 60rpx;
+	
+}
+.mt-subject {
+	width: 100%;
+	background-color: #FFFFFF;
+	margin-bottom: 30rpx;
+}
+.mt-subject1 {
+	width: 100%;
+	background-color: #FFFFFF;
+	height: 250rpx;
+	/* margin-bottom: 10rpx; */
+}
+.mt-city {
+	width: 20rpx;
+	height: 20rpx;
+	margin: 0 10rpx;
+	
+}
+.mt-buttons-content {
+	position: relative;
+}
+.mt-order-img {
+	left:30rpx;
+	top:50%;
+	transform: translateY(-30%);
+}
+.mt-order {
+	position: absolute;
+	lefe:50%;
+	top:50%;
+	transform: translate(-50%,-50%);
+}
 .content {
 	display: flex;
 	flex-direction: column;
@@ -63,43 +241,62 @@ export default {
 	justify-content: center;
 	color: #333333;
 }
-
-.uni-padding-wrap {
+.swiper {
 	width: 100%;
-	height: 200px;
-
+	height: 294rpx;
 	background-color: #007aff;
 }
-.swiper-item {
+.swiper-item image {
 	width: 100%;
-	height: 100%;
+	height: 294rpx;
 }
-.mt-buttons-box {
+.mt-buttons-content {
 	display: flex;
-	width: 100%;
-	height: 166rpx;
-	align-items: center;
-	justify-content: center;
-	box-sizing: border-box;
-}
-
-.mt-button-item {
-	flex: 1;
 	flex-direction: row;
-	align-items: center;
 	justify-content: center;
-	line-height: 51px;
-	text-align: center;
+	width: 100%;
+	height: 110.66rpx;
+	background-color: #FFFFFF;
+	font-size:29.33rpx ;
+}
+.current-active {
+	color:#1A4397;
+}
+.mt-buttons-item{
+	  flex: 1;
+		justify-content: center;
+		text-align: center;
 }
 .mt-image-content{
-	float: left;
-	width: 83rpx;
-	height:103rpx;
-	vertical-align: middle;
+	width:54rpx ;
+	height: 68.66rpx;
+}
+.second-image {
+	width: 62rpx;
+	
 }
 .mt-image-content image{
 	width: 100%;
-	height:100%;
-	vertical-align: middle;
+	height: 100%;
+}
+.mt-place-content {
+	width: 0.66rpx;
+	height: 54.66rpx;
+	background-color: #cccccc;
+}
+.mt-throw-content{
+	width: 100%;
+	box-sizing: border-box;
+	padding:10rpx;
+}
+.mt-start-image {
+		width: 100%;
+		background-color: #fff;
+		
+	}
+.mt-start-edit{
+	width: 37.33rpx;
+	height: 41.33rpx;
+	color: #007AFF;
 }
 </style>
