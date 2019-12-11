da398a3f93f1154172aa3d970d68e266204f9fb8
guoxj <guoxj@miiteen.com>
2019-12-11 22:05:47 +0800

    增加alert组件

---
+++ pages/demo/demo.vue
@@ -0,0 +1,39 @@
+<template>
+	<view>
+		<mt-alert ref="mtalert" content="保存成功" @change="change"> </mt-alert>
+	</view>
+</template>
+
+<script>
+	import mtAlert from '@/components/miiteen/popups/mt-alert.vue'
+	export default {
+		name: "MtAlert",
+		components: {
+			mtAlert
+		},
+		props: {
+			//内容
+			content: {
+				type: String,
+				default: ""
+			}
+		},
+		data() {
+			return {
+
+			}
+		},
+		mounted: function() {
+			this.$refs["mtalert"].open();
+		},
+		methods: {
+			change: function(e) {
+				console.log(e)
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
