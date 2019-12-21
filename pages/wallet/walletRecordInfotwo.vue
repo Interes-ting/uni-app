5b8ba578774c9911714b883c02700a3686158a84
maxd <maxd@miiiteen.com>
2019-12-21 12:15:53 +0800

    。

--- pages/wallet/walletRecordInfotwo.vue
+++ pages/wallet/walletRecordInfotwo.vue
@@ -12,24 +12,27 @@
 					</view>
 					<view class="mt-startcity">
 						<view class="mt-placebox"></view>
+						<text class="text-grey">联系电话：{{list.phone}}</text>
+					</view>
+					<view class="mt-startcity">
+						<view class="mt-placebox"></view>
 						<text class="text-grey">申请时间：{{list.applyTime}}</text>
 					</view>
 					<view class="mt-startcity" >
 						<view class="mt-placebox"></view>
 						<text class="text-grey">提现卡号：{{list.cardNo}}</text>
 					</view>
+					
 					<view class="mt-startcity">
 						<view class="mt-placebox"></view>
 						<text class="text-grey">提现开户行：{{list.cardBank}}</text>
 					</view>
-					<view class="mt-startcity">
-						<view class="mt-placebox"></view>
-						<text class="text-grey">提现人姓名：{{list.names}}</text>
+					<!-- 注意事项 -->
+					<view class="mt-startcity" v-if="list.names" style="display: inline-block;">
+						<view class="text-grey" style="float: left;">提现人姓名：</view>
+						<view class="text-grey" style="float: left;word-break: break-all;width: 74%;">{{ list.names }}</view>
 					</view>
-					<view class="mt-startcity">
-						<view class="mt-placebox"></view>
-						<text class="text-grey">联系电话：{{list.phone}}</text>
-					</view>
+					
 				</view>
 				
 			</view>
@@ -103,112 +106,107 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.OrderInfo-content {
-	padding: 13.33rpx;
-
-	color: #333333;
-}
-
-.space {
-	letter-spacing: 6.66rpx;
-}
-.bold {
-	font-weight: 700;
-	font-size: 32rpx;
-}
-.text-grey {
-	color: #999999;
-	font-size: 25.52rpx;
-}
-page{
-	background-color: #FFFFFF;
-}
-.mt-card {
-	position: relative;
-	padding: 13.33rpx;
-	background-color: #ffffff;
-	box-shadow: 0px 0px 25.33rpx 0px rgba(0, 0, 0, 0.08);
-	border-radius: 13.33rpx;
-	.mt-city{
-		padding-top: 13.33rpx;
-	}
-	.mt-startcity {
-		display: block;
-		height: 50rpx;
-		line-height: 50rpx;
-		width: 100%;
-		font-size: 25.52rpx;
+.mt-kehuname {
+		display: flex;
+		margin: 0rpx 40rpx;
+		padding: 20rpx 0rpx;
+		
+		.mt-name {
+			display: flex;
+			flex-flow: column;
+			line-height: 80rpx;
+			.mt-textphone {
+				width: 400rpx;
+			}
+		}
 		.mtfa {
-			display: inline-block;
-			font-size: 26.66rpx;
-			margin-right: 10rpx;
+			font-size: 60rpx;
+			color: #37932A;
+			margin-right: 30rpx;
 		}
-		.mt-placebox {
-			display: inline-block;
-			width: 36.66rpx;
+		.mt-callphone {
+			flex: 1;
+			text-align: right;
 		}
 	}
-	page: {
-		background-color: #FFFFFF;
-	};
-	.mt-viewbutton {
-		position: absolute;
-		right: 26.66rpx;
-		bottom: 20rpx;
-		width: 124rpx;
-		height: 53.33rpx;
-		line-height: 53.33rpx;
-		background: linear-gradient(to bottom, #1880ff, #509eff);
-		/*设置按钮为渐变颜色*/
-		border-radius: 26.66rpx;
-		font-size: 24rpx;
-		color: #ffffff;
+	.mt-kehuphone {
+		text-indent: 40rpx;
+		font-size: 30rpx;
+		color: #ff571d;
+		font-weight: 900;
+		letter-spacing: 5rpx;
+		padding: 20rpx 0rpx;
+	}
+	.text-remark-text {
+		flex: 1;
+		overflow: hidden;
+		white-space: nowrap;
+		text-overflow: ellipsis;
+		display: inline-block;
+		width: 100%;
+		height: 100;
+	}
+	.text-remark {
+		display: inline-block;
+		width: 120rpx;
+	}
+	
+	.oderList-content {
+		padding: 20rpx;
+	
+		color: #333333;
+	}
+	.space {
+		letter-spacing: 6.66rpx;
+	}
+	.bold {
+		font-weight: 700;
+		font-size: 32rpx;
+	}
+	.text-grey {
+		color: #999999;
+		font-size: 25.52rpx;
+	}
+	.mt-card {
+		position: relative;
+		padding: 13.33rpx;
+		background-color: #ffffff;
+		box-shadow: 0px 0px 25.33rpx 0px rgba(0, 0, 0, 0.08);
+		border-radius: 13.33rpx;
+		.mt-city {
+			padding-top: 13.33rpx;
+		}
+		.mt-startcity {
+			display: block;
+			line-height: 50rpx;
+			width: 100%;
+			font-size: 25.52rpx;
+			.mtfa {
+				display: inline-block;
+				font-size: 26.66rpx;
+				margin-right: 10rpx;
+			}
+			.mt-placebox {
+				display: inline-block;
+				width: 36.66rpx;
+			}
+		}
+		.mt-viewbutton {
+			position: absolute;
+			right: 26.66rpx;
+			bottom: 20rpx;
+			width: 124rpx;
+			height: 53.33rpx;
+			line-height: 53.33rpx;
+			background: linear-gradient(to bottom, #1880ff, #509eff);
+			/*设置按钮为渐变颜色*/
+			border-radius: 26.66rpx;
+			font-size: 24rpx;
+			color: #ffffff;
+		}
 	}
 	.mt-oderinfo {
 		padding: 13.33rpx 0rpx;
 		border-bottom: 1px solid #cccccc;
 	}
-}
-.mt-cardtwo {
-	position: relative;
-	padding: 13.33rpx;
-	background-color: #ffffff;
-	.mt-city{
-		padding-top: 13.33rpx;
-	}
-	
-	.mt-startcity {
-		display: block;
-		height: 50rpx;
-		line-height: 50rpx;
-		width: 100%;
-		font-size: 25.52rpx;
-		.mtfa {
-			display: inline-block;
-			font-size: 26.66rpx;
-			margin-right: 10rpx;
-		}
-		.mt-placebox {
-			display: inline-block;
-			width: 36.66rpx;
-		}
-	}
-	.mt-viewbutton {
-		position: absolute;
-		right: 26.66rpx;
-		bottom: 20rpx;
-		width: 124rpx;
-		height: 53.33rpx;
-		line-height: 53.33rpx;
-		background: linear-gradient(to bottom, #1880ff, #509eff);
-		/*设置按钮为渐变颜色*/
-		border-radius: 26.66rpx;
-		font-size: 24rpx;
-		color: #ffffff;
-	}
-	
-}
-.oderList-content.heis{
-	height: 100%;
-}
 </style>
